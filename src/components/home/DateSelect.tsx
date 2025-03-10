060cdd33914c305d61937c6ab5d61f0938714f36
incolore9 <hansol.woff@gmail.com>
2025-03-10 10:28:57 +0900

    fix: 날짜 오류 및 표기 형식 수정 / 날짜 미지정 상태 수정

--- src/components/home/DateSelect.tsx
+++ src/components/home/DateSelect.tsx
@@ -11,21 +11,28 @@ import { Calendar } from "@/components/ui/calendar";
 import { useState } from "react";
 
 export default function DateSelect() {
-  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [date, setDate] = useState<Date | undefined>(undefined);
+
+  const formatDate = (date: Date | undefined) => {
+    if (!date) {
+      return "날짜 선택";
+    } else {
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
+      return `${date.getFullYear()}/${month}/${day}`;
+    }
+  };
 
   return (
     <>
       <Select>
         <SelectTrigger className="w-auto">
-          <SelectValue
-            placeholder={date ? date.toLocaleDateString() : "날짜 선택"}
-          />
+          <SelectValue placeholder={date ? formatDate(date) : "날짜 선택"} />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
             <Calendar
               mode="single"
-              selected={date}
               onSelect={setDate}
               className="rounded-md border shadow"
             />
