9220b34bdc201f5133bb0651d66571f6c7e71c9e
incolore9 <hansol.woff@gmail.com>
2025-03-10 12:53:27 +0900

    design: 모임 만들기 인풋 폼 배치

--- src/components/home/AddGroupButtonModal.tsx
+++ src/components/home/AddGroupButtonModal.tsx
@@ -3,7 +3,6 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -16,6 +15,11 @@ import { useState } from "react";
 
 export function AddGroupButtonModal() {
   let [groupName, setGroupName] = useState("");
+  let [groupLocation, setGroupLocation] = useState("");
+  let [groupImage, setGroupImage] = useState("");
+  let [groupHeadcount, setGroupHeadcount] = useState("");
+
+  const handleSubmit = function () {};
 
   return (
     <Dialog>
@@ -27,29 +31,94 @@ export function AddGroupButtonModal() {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>모임 만들기</DialogTitle>
-          {/* <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription> */}
         </DialogHeader>
-        <div className="flex">
-          <div className="flex-col gap-4">
-            <Label htmlFor="groupName" className="mb-2">
-              모임 이름
-            </Label>
-            <Input
-              id="name"
-              value={groupName}
-              onChange={(e) => {
-                setGroupName(e.currentTarget.value);
-              }}
-              className="w-full"
-              placeholder="모임 이름을 작성해주세요."
-            />
+        <form onSubmit={handleSubmit}>
+          <div className="flex w-full flex-col gap-4">
+            <div>
+              <Label htmlFor="groupName" className="mb-2">
+                모임 이름
+              </Label>
+              <Input
+                id="name"
+                value={groupName}
+                onChange={(e) => {
+                  setGroupName(e.currentTarget.value);
+                }}
+                className="w-full"
+                placeholder="모임 이름을 작성해주세요."
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="groupName" className="mb-2">
+                장소
+              </Label>
+              <Input
+                id="location"
+                type="select"
+                value={groupLocation}
+                onChange={(e) => {
+                  setGroupLocation(e.currentTarget.value);
+                }}
+                className="w-full"
+                placeholder="장소를 선택해주세요."
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="groupName" className="mb-2">
+                이미지
+              </Label>
+              <div className="flex flex-row gap-4">
+                <Input
+                  id="fileName"
+                  value={groupImage}
+                  placeholder="이미지를 첨부해주세요."
+                  className="w-3/4"
+                  readOnly
+                />
+                <div className="relative w-1/4">
+                  <Input
+                    id="fileInput"
+                    type="file"
+                    onChange={(e) => {
+                      setGroupImage(e.currentTarget.value);
+                    }}
+                    className="absolute inset-0 w-full cursor-pointer opacity-0"
+                    accept="image/*"
+                  />
+                  <Button type="button" className="w-full">
+                    파일 선택
+                  </Button>
+                </div>
+              </div>
+            </div>
+            <div>
+              <Label htmlFor="headCount" className="mb-2">
+                모집 정원
+              </Label>
+              <Input
+                id="headCount"
+                value={groupHeadcount}
+                type="number"
+                onChange={(e) => {
+                  setGroupHeadcount(e.currentTarget.value);
+                }}
+                className="w-full [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                placeholder="최소 5인 이상 입력해주세요"
+              />
+            </div>
           </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button
+              onClick={() => {
+                handleSubmit();
+              }}
+            >
+              Save changes
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
